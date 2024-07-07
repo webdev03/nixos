c51344e43dc183a3885e8d027ba8c8763b328deb
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-07-07 14:32:24 +1200

    attempt to fix

--- nixosModules/desktop/hyprland.nix
+++ nixosModules/desktop/hyprland.nix
@@ -9,6 +9,7 @@
   };
   config = lib.mkIf config.desktop.hyprland.enable {
     services.xserver.enable = true;
+    services.xserver.displayManager.startx.enable = true;
     # services.displayManager.sddm.enable = true;
     # services.displayManager.sddm.wayland.enable = true;
     programs.hyprland.enable = true;
