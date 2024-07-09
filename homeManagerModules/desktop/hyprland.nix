a1fd1fb6da9dc3168afde67e3b19476b7da4be45
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-07-10 10:13:11 +1200

    change hyprland settings

--- homeManagerModules/desktop/hyprland.nix
+++ homeManagerModules/desktop/hyprland.nix
@@ -84,6 +84,9 @@
           kb_layout = "us";
           follow_mouse = "1";
           sensitivity = "0";
+          touchpad = {
+            natural_scroll = true;
+          };
         };
         gestures = {
           workspace_swipe = "false";
@@ -114,12 +117,20 @@
           "$mainMod, 3, workspace, 3"
           "$mainMod, 4, workspace, 4"
           "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
 
           "$mainMod SHIFT, 1, movetoworkspace, 1"
           "$mainMod SHIFT, 2, movetoworkspace, 2"
           "$mainMod SHIFT, 3, movetoworkspace, 3"
           "$mainMod SHIFT, 4, movetoworkspace, 4"
           "$mainMod SHIFT, 5, movetoworkspace, 5"
+          "$mainMod SHIFT, 6, movetoworkspace, 6"
+          "$mainMod SHIFT, 7, movetoworkspace, 7"
+          "$mainMod SHIFT, 8, movetoworkspace, 8"
+          "$mainMod SHIFT, 9, movetoworkspace, 9"
         ];
         # SUPER + Left or Right mouse button to move or resize a window respectively
         bindm = [
