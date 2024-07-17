74f22047245e2daf8fc825ee2b490585aaffd528
webdev03 <75148774+webdev03@users.noreply.github.com>
2024-07-17 21:03:19 +1200

    fix pipewire thing

--- nixosModules/system/pipewire.nix
+++ nixosModules/system/pipewire.nix
@@ -9,7 +9,6 @@
   };
   config = lib.mkIf config.system.pipewire.enable {
     security.rtkit.enable = true;
-    sound.enable = lib.mkForce false; # disable alsa
     hardware.pulseaudio.enable = lib.mkForce false; # disable pulseaudio
     services.pipewire = {
       enable = true;
